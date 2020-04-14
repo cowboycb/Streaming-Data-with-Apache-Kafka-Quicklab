9314088a24520427185684ea02205d284d36ac2b
prpatel <pratik@mypatelspace.com>
2020-04-14 10:16:01 -0400

    cleaned up imports

--- src/main/java/org/acme/WindSpeedResource.java
+++ src/main/java/org/acme/WindSpeedResource.java
@@ -1,15 +1,14 @@
 package org.acme;
 
 import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.resteasy.annotations.SseElementType;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.jboss.resteasy.annotations.SseElementType;
 
 @Path("/windSpeed")
 public class WindSpeedResource {
